5d1497e123950442c76b0801414c05f3fb81ad2a
Zxander <Zxander.Rodriguez@cognizant.com>
2021-05-08 11:25:18 -0500

    RED - post company test

--- src/test/java/com/hundred/percent/capstone/Invoicify/company/CompanyIT.java
+++ src/test/java/com/hundred/percent/capstone/Invoicify/company/CompanyIT.java
@@ -1,14 +1,17 @@
 package com.hundred.percent.capstone.Invoicify.company;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,11 +24,23 @@ public class CompanyIT {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void getEmptyCompanyTest() throws Exception {
         mockMvc.perform(get("/companies"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("length()").value(0))
+                .andDo(print());
+    }
+
+    @Test
+    public void postCompanyTest() throws Exception {
+        mockMvc.perform(post("/companies/addCompany")
+                .content(objectMapper.writeValueAsString(""))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
                 .andDo(print());
     }
 }
