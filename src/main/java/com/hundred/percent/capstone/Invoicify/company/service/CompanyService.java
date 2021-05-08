5495f11cb1460f7e5d483e382af1f6fdffe848c6
Zxander <Zxander.Rodriguez@cognizant.com>
2021-05-08 12:05:24 -0500

    GREEN service create company test

--- src/main/java/com/hundred/percent/capstone/Invoicify/company/service/CompanyService.java
+++ src/main/java/com/hundred/percent/capstone/Invoicify/company/service/CompanyService.java
@@ -1,11 +1,18 @@
 package com.hundred.percent.capstone.Invoicify.company.service;
 
 import com.hundred.percent.capstone.Invoicify.company.dto.CompanyDTO;
+import com.hundred.percent.capstone.Invoicify.company.entity.CompanyEntity;
+import com.hundred.percent.capstone.Invoicify.company.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompanyService {
 
+    @Autowired
+    CompanyRepository repository;
+
     public void createCompany(CompanyDTO companyDTO) {
+        repository.save(new CompanyEntity(companyDTO.getName(), companyDTO.getAddress(), companyDTO.getContact_name(), companyDTO.getContact_title(), companyDTO.getContact_phone_number()));
     }
 }
