35270a5a5955be8c12cd8ba9cf8287bfd2f7bec5
sunita1501 <sunita.kumari@cognizant.com>
2021-05-14 19:31:49 -0400

    Red on get invoice by invoice number

--- src/test/java/com/hundred/percent/capstone/Invoicify/invoice/InvoiceIT.java
+++ src/test/java/com/hundred/percent/capstone/Invoicify/invoice/InvoiceIT.java
@@ -139,4 +139,61 @@ public class InvoiceIT {
                 .andDo(document("getInvoice"));
 
     }
+
+    @Test
+    public void getInvoicesByCompanyInvoiceNumber() throws Exception{
+        List<ItemDTO> itemsDTO1 = new ArrayList<ItemDTO>();
+        itemsDTO1.add(new ItemDTO("Brand Website Customization",1000));
+        itemsDTO1.add(new ItemDTO("Brand Website Customization",20));
+        itemsDTO1.add(new ItemDTO("Product Pages",20,3));
+
+        InvoiceDTO d1=new InvoiceDTO(1, itemsDTO1);
+
+        List<ItemDTO> itemsDTO2 = new ArrayList<ItemDTO>();
+        itemsDTO2.add(new ItemDTO("Item1",2000));
+        itemsDTO2.add(new ItemDTO("Item1",40));
+        itemsDTO2.add(new ItemDTO("Item2",40,3));
+
+        InvoiceDTO d2=new InvoiceDTO(2, itemsDTO1);
+
+        mockMvc.perform(post("/invoices")
+                .content(objectMapper.writeValueAsString(d1))
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isCreated())
+                .andDo(document("postInvoice"));
+
+        mockMvc.perform(post("/invoices")
+                .content(objectMapper.writeValueAsString(d2))
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isCreated())
+                .andDo(document("postInvoice"));
+
+        mockMvc.perform(get("/invoices/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("length()").value(1))
+                .andExpect(jsonPath("$.[0].invoiceNumber").value("1"))
+                .andExpect(jsonPath("$.[0].items.[0].description").value("Brand Website Customization"))
+                .andExpect(jsonPath("$.[0].items.[0].price").value("1000"))
+                .andExpect(jsonPath("$.[0].items.[0].feeType").value("FlatFee"))
+                .andExpect(jsonPath("$.[0].items.[0].quantity").value("1"))
+                .andExpect(jsonPath("$.[0].items.[0].fee").value("1000"))
+
+                .andExpect(jsonPath("$.[0].invoiceNumber").value("1"))
+                .andExpect(jsonPath("$.[0].items.[1].description").value("Brand Website Customization"))
+                .andExpect(jsonPath("$.[0].items.[1].price").value("20"))
+                .andExpect(jsonPath("$.[0].items.[1].feeType").value("FlatFee"))
+                .andExpect(jsonPath("$.[0].items.[1].quantity").value("1"))
+                .andExpect(jsonPath("$.[0].items.[1].fee").value("20"))
+
+                .andExpect(jsonPath("$.[0].invoiceNumber").value("1"))
+                .andExpect(jsonPath("$.[0].items.[2].description").value("Product Pages"))
+                .andExpect(jsonPath("$.[0].items.[2].price").value("20"))
+                .andExpect(jsonPath("$.[0].items.[2].feeType").value("RateBased"))
+                .andExpect(jsonPath("$.[0].items.[2].quantity").value("3"))
+                .andExpect(jsonPath("$.[0].items.[2].fee").value("60"))
+                .andDo(document("getInvoice"));
+
+    }
+
+
 }
