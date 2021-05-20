6295f91305d0ed63a9d08d309678c6150ab411cc
404-bug-notfound <iqbal.jamadar@gmail.com>
2021-05-19 21:54:04 -0400

    Added employee entity, repository, service and controller

---
+++ src/main/java/com/hundred/percent/capstone/Invoicify/Employee/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.hundred.percent.capstone.Invoicify.Employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+  @Autowired
+  EmployeeService employeeService;
+
+  @PostMapping
+  public Employee createEmployee(@RequestBody Employee employee) {
+    return employeeService.save(employee);
+  }
+
+  @GetMapping("/{id}")
+  public Optional<Employee> getById(@PathVariable(value = "id") UUID id) {
+    return employeeService.get(id);
+  }
+
+}
---
+++ src/main/java/com/hundred/percent/capstone/Invoicify/Employee/EmployeeService.java
@@ -0,0 +1,34 @@
+package com.hundred.percent.capstone.Invoicify.Employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class EmployeeService {
+
+  @Autowired
+  EmployeeRepository repository;
+
+  @Autowired
+  PasswordEncoder passwordEncoder;
+
+  public List<Employee> getAll() {
+    return repository.findAll();
+  }
+
+  public Optional<Employee> get(UUID id) {
+    return repository.findById(id);
+  }
+
+  public Employee save(Employee employee) {
+    employee.setPassword(passwordEncoder.encode(employee.getPassword()));
+    return repository.save(employee);
+  }
+}
